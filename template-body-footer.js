f63726c63865fb7467af2f2870f80cd67aa7032b
ifc-jo <63009511+ifc-jo@users.noreply.github.com>
2020-06-30 22:58:10 +0100

    Add files via upload

---
+++ template-body-footer.js
@@ -0,0 +1,15 @@
+document.write(`
+
+  <footer id="footer">
+    <p>Rua das Forças Armadas, 90 - 1º Dto.</p>
+    <p>4715-029 Braga</p>
+    <p><i class="fas fa-mobile-alt"></i>&nbsp; 253 268 381</p>
+    <a href="mailto:geral@infoconsul.pt" title="geral@infoconsul.pt">geral@infoconsul.pt</a>
+    <!-- <p>geral@infoconsul.pt</p> -->
+    <p>
+      <p>
+        <p>© InfoConsul</p>
+  </footer>
+
+
+  `)
---
+++ template-body-top.js
@@ -0,0 +1,40 @@
+document.write(`
+
+  <section id="menu">
+    <div class="container-fluid ifc-container-menu">
+      <!-- Nav Bar -->
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="index.html">
+          <img src="images/logo-250-78.png" alt="">
+        </a>
+          <ul class="navbar-nav ml-auto">
+            <!-- <ul class="navbar-nav ml-left"> -->
+            <li class="nav-item">
+              <a class="nav-link teste-01" href="index.html">Home</a>
+            </li>
+          </ul>
+
+      </nav>
+
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="http://www.infoconsul.pt/" target="_blank">Empresa</a>
+        <a href="#">Parcerias</a>
+        <a href="#">Serviços</a>
+        <a href="#">Contactos</a>
+      </div>
+
+    </div>
+  </section>
+
+  <div class="container-fluid ifc-container-logo-title">
+    <section id="title">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <span>Especialistas em Software de Gestão</span>
+        </div>
+      </div>
+    </section>
+  </div>
+
+`);
